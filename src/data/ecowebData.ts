57dfdb24b84e11735ae0de3cb3ab1f1b60cceb04
Golen <golen87@hotmail.com>
2021-05-31 21:14:13 +0200

    Add preloading and node data.

---
+++ src/data/ecowebData.ts
@@ -0,0 +1,27 @@
+import { prependPath } from "../utils";
+
+
+const nodeImages = prependPath("assets/images/ecoweb/128/", [
+	// Animals
+	{ key: "Blafjaril.png",				path: "Blafjaril.png" },
+	{ key: "ConnochaetesTaurinus.png",	path: "Connochaetes taurinus.png" },
+	{ key: "CrocutaCrocuta.png",		path: "Crocuta crocuta.png" },
+	{ key: "EquusQuagga.png",			path: "Equus quagga.png" },
+	{ key: "LoxodontaAfricana.png",		path: "Loxodonta africana.png" },
+	{ key: "LycaonPicturus.png",		path: "Lycaon picturus.png" },
+	{ key: "MadoquaKirkii.png",			path: "Madoqua kirkii.png" },
+	{ key: "PantheraLeo.png",			path: "Panthera leo.png" },
+	{ key: "Rapsbagge.png",				path: "Rapsbagge.png" },
+	{ key: "RhabdomysPumilio.png",		path: "Rhabdomys pumilio.png" },
+	{ key: "Serval.png",				path: "Serval.png" },
+
+	// Plants
+	{ key: "AcalyphaFruticosa.png",		path: "Acalypha fruticosa.png" },
+	{ key: "HeteropogonContortus.png",	path: "Heteropogon contortus.png" },
+	{ key: "Kummin.png",				path: "Kummin.png" },
+	{ key: "Raps.png",					path: "Raps.png" },
+	{ key: "ThemedaTriandra.png",		path: "Themeda triandra.png" },
+]);
+
+
+export { nodeImages };
--- src/game.ts
+++ src/game.ts
@@ -1,11 +1,15 @@
 import 'phaser';
 import { MainScene } from './scenes/mainScene';
+import { PreloadScene } from './scenes/PreloadScene';
 
 const config: GameConfig = {
 	type: Phaser.AUTO,
 	width: 800,
 	height: 600,
-	scene: [MainScene],
+	scene: [
+		PreloadScene,
+		MainScene
+	],
 };
 
 const game = new Phaser.Game(config);
